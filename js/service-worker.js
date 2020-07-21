2da05e2896d722a0271435c09337dd582be1e8b1
meponderR <33591888+meponderR@users.noreply.github.com>
2020-07-21 04:12:35 -0500

    Create Website

---
+++ js/service-worker.js
@@ -0,0 +1,21 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+/*
+import {registerRoute} from 'workbox-routing';
+import {NetworkFirst} from 'workbox-strategies';
+
+registerRoute(
+  ({request}) => request.destination === 'script',
+  new NetworkFirst()
+);
+
+registerRoute(
+  ({request}) => request.destination === 'style',
+  new NetworkFirst()
+);
+*/
