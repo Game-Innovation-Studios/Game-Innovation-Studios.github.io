0d3f94ffc2912c4b6e34fca32153cb156f5fa7ec
meponderR <33591888+meponderR@users.noreply.github.com>
2020-07-22 00:42:53 -0500

    Use newer SW

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,64 +1,28 @@
-// sw.js
+'use strict';
 
-// CACHE_NAMESPACE
-// CacheStorage is shared between all sites under same domain.
-// A namespace can prevent potential name conflicts and mis-deletion.
-const CACHE_NAMESPACE = 'mmpwa-'
+// CODELAB: Update cache names any time any of the cached files change.
+const CACHE_NAME = 'static-cache-v1';
 
-const PRECACHE = CACHE_NAMESPACE + 'precache-v3'
-const PRECACHE_LIST = [
-  './offline.html'
-]
-const RUNTIME = CACHE_NAMESPACE + 'runtime-v1'
-const expectedCaches = [PRECACHE, RUNTIME]
+// CODELAB: Add list of files to cache here.
+const FILES_TO_CACHE = [
+];
 
+self.addEventListener('install', (evt) => {
+  console.log('[ServiceWorker] Install');
+  // CODELAB: Precache static resources here.
 
-self.oninstall = (event) => {
-  event.waitUntil(
-    caches.open(PRECACHE)
-    .then(cache => cache.addAll(PRECACHE_LIST))
-    .then(self.skipWaiting())
-    .catch(err => console.log(err))
-  )
-}
+  self.skipWaiting();
+});
 
-self.onactivate = (event) => {
-  // delete any cache not match expectedCaches for migration.
-  // noticed that we delete by cache instead of by request here.
-  // so we MUST filter out caches opened by this app firstly.
-  // check out sw-precache or workbox-build for an better way.
-  event.waitUntil(
-    caches.keys().then(cacheNames => Promise.all(
-      cacheNames
-        .filter(cacheName => cacheName.startsWith(CACHE_NAMESPACE))
-        .filter(cacheName => !expectedCaches.includes(cacheName))
-        .map(cacheName => caches.delete(cacheName))
-    ))
-  )
-}
+self.addEventListener('activate', (evt) => {
+  console.log('[ServiceWorker] Activate');
+  // CODELAB: Remove previous cached data from disk.
 
-self.onfetch = (event) => {
-  // Fastest-while-revalidate
-  const cached = caches.match(event.request);
-  const fixedUrl = `${event.request.url}?${Date.now()}`;
-  const fetched = fetch(fixedUrl, {cache: "no-store"});
-  const fetchedCopy = fetched.then(resp => resp.clone());
-  console.log(`fetch ${fixedUrl}`)
+  self.clients.claim();
+});
 
-  // Call respondWith() with whatever we get first.
-  // If the fetch fails (e.g disconnected), wait for the cache.
-  // If there’s nothing in cache, wait for the fetch.
-  // If neither yields a response, return offline pages.
-  event.respondWith(
-    Promise.race([fetched.catch(_ => cached), cached])
-      .then(resp => resp || fetched)
-      .catch(_ => caches.match('offline.html'))
-  );
+self.addEventListener('fetch', (evt) => {
+  console.log('[ServiceWorker] Fetch', evt.request.url);
+  // CODELAB: Add fetch event handler here.
 
-  // Update the cache with the version we fetched (only for ok status)
-  event.waitUntil(
-    Promise.all([fetchedCopy, caches.open(RUNTIME)])
-      .then(([response, cache]) => response.ok && cache.put(event.request, response))
-      .catch(_ => {/* eat any errors */})
-  );
-}
+});
